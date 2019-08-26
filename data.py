a2a437dcd0b0c69d288b211d742150b9ea9930eb
ebucheli <edoardobucheli@gmail.com>
2019-08-26 00:00:50 -0500

    choose representation and sr

--- data.py
+++ data.py
@@ -15,7 +15,18 @@ path_ds = '/home/edoardobucheli/Datasets/freesound-audio-tagging-2019/'
 batch_size = 64
 sr=44100
 
-def preprocess_wave(file):
+def preprocess_wave(file,
+                    transformation,
+                    normalize,
+                    sr,
+                    len_sec,
+                    frame_length,
+                    frame_step,
+                    fft_length,
+                    n_mels,
+                    n_mfcc,
+                    mel_lower_edge,
+                    mel_upper_edge):
 
     def log10(x):
         numerator = tf.log(x)
@@ -30,56 +41,133 @@ def preprocess_wave(file):
     if tf.__version__ == '1.12.0':
         rate = tf.constant(44100, dtype = tf.int32)
         wave = tf.contrib.ffmpeg.decode_audio(file,file_format='wav',
-                                samples_per_second=44100,channel_count=1)
+                                samples_per_second=sr,channel_count=1)
     elif tf.__version__ == '1.14.0':
         wave,rate = tf.audio.decode_wav(file, desired_channels = 1, desired_samples = -1)
 
+    duration = tf.constant(sr*len_sec,tf.int32)
+
     size1 = tf.shape(wave)[0]
-    diff = rate-size1
+    #diff = rate-size1
+    diff = duration-size1
     diff = tf.cond(diff < 0, lambda:0,lambda:diff)
 
     pad = tf.zeros((diff,1))
     wave = tf.concat((wave,pad),axis=0)
     size2 = tf.shape(wave)[0]
-    max_start = size2-rate+1
+    max_start = size2-duration+1
 
     start = tf.random_uniform((), minval=0, maxval=max_start,
                               dtype=tf.int32, seed=None, name=None)
 
-    x = wave[start:start+44100,:]
+    x = wave[start:start+duration,:]
     x = tf.expand_dims(x,axis = 0)
     x = tf.squeeze(x,axis = -1)
     x = x/tf.reduce_max(tf.abs(x),axis = 1)
 
-    X = tf.contrib.signal.stft(x,frame_length=512,
-                               frame_step = 256, fft_length = 1024)
-    mX = twenty*log10(tf.abs(X))
-    pX = tf.angle(X)
+    if transformation == 'wave':
+        rep = x
 
-    mel_matrix = tf.contrib.signal.linear_to_mel_weight_matrix(num_mel_bins = 120,
-                                             num_spectrogram_bins=513,
-                                             sample_rate = 44100,
-                                             lower_edge_hertz=125,
-                                             upper_edge_hertz=16384)
-    mel_S = tf.matmul(tf.squeeze(mX,axis=0),mel_matrix)
-    mfccs = tf.contrib.signal.mfccs_from_log_mel_spectrograms(mel_S)[..., :20]
+    else:
 
-    melmin = tf.reduce_min(mel_S)
-    melmax = tf.reduce_max(mel_S)
-    half_len = (melmax-melmin)/2
-    melmid = melmin+half_len
+        X = tf.contrib.signal.stft(x,
+                                   frame_length = frame_length,
+                                   frame_step = frame_step,
+                                   fft_length = fft_length)
 
-    mel_S -= melmid
-    mel_S /= tf.reduce_max(tf.abs(mel_S))
+    #pX = tf.angle(X)
 
+    if transformation == 'mag':
+        rep = twenty*log10(tf.abs(X))
 
-    return x,mX,pX,mel_S,mfccs
+        if normalize:
+            mXmax = tf.reduce_max(rep)
+            rep = tf.where(tf.is_inf(rep),-rep,rep)
+            mXmin = tf.reduce_min(rep)
 
-def load_and_preprocess_wav(path):
+            rep = tf.where(tf.is_inf(rep),tf.ones_like(rep)*mXmin,rep)
+
+            mX_half_len = (mXmax-mXmin)/2
+            mXmid = mXmin+mX_half_len
+
+            rep -= mXmid
+            rep /= tf.reduce_max(tf.abs(rep))
+
+    elif transformation == 'mel':
+        mel_matrix = tf.contrib.signal.linear_to_mel_weight_matrix(num_mel_bins = n_mels,
+                                                 num_spectrogram_bins = int(fft_length/2+1),
+                                                 sample_rate = sr,
+                                                 lower_edge_hertz = mel_lower_edge,
+                                                 upper_edge_hertz = mel_upper_edge)
+
+        mX = twenty*log10(tf.abs(X))
+        rep = tf.matmul(tf.squeeze(mX,axis=0),mel_matrix)
+
+        if normalize:
+            melmin = tf.reduce_min(rep)
+            melmax = tf.reduce_max(rep)
+            half_len = (melmax-melmin)/2
+            melmid = melmin+half_len
+
+            rep -= melmid
+            rep /= tf.reduce_max(tf.abs(rep))
+
+            rep = tf.where(tf.is_nan(rep),tf.ones_like(rep)*-1,rep)
+
+    elif transformation == 'mfcc':
+        mel_matrix = tf.contrib.signal.linear_to_mel_weight_matrix(num_mel_bins = n_mels,
+                                                 num_spectrogram_bins = int(fft_length/2+1),
+                                                 sample_rate = sr,
+                                                 lower_edge_hertz = mel_lower_edge,
+                                                 upper_edge_hertz = mel_upper_edge)
+        mX = twenty*log10(tf.abs(X))
+        mel_S = tf.matmul(tf.squeeze(mX,axis=0),mel_matrix)
+        rep = tf.contrib.signal.mfccs_from_log_mel_spectrograms(mel_S)[..., :n_mfcc]
+
+    return rep
+
+def load_and_preprocess_wav(path,
+                            transformation,
+                            normalize,
+                            sr,
+                            len_sec,
+                            frame_length,
+                            frame_step,
+                            fft_length,
+                            n_mels,
+                            n_mfcc,
+                            mel_lower_edge,
+                            mel_upper_edge):
+
+    _preprocess_wave = lambda _file: preprocess_wave(_file,
+                                                     transformation,
+                                                     normalize,
+                                                     sr,
+                                                     len_sec,
+                                                     frame_length,
+                                                     frame_step,
+                                                     fft_length,
+                                                     n_mels,
+                                                     n_mfcc,
+                                                     mel_lower_edge,
+                                                     mel_upper_edge)
+
     file = tf.read_file(path)
-    return preprocess_wave(file)
+    return _preprocess_wave(file)
 
-def create_and_batch_dataset_freesound(path_ds, part='curated', batch_size=32, sr=44100):
+def create_and_batch_dataset_freesound(path_ds, part='curated',
+                                       batch_size=32,
+                                       transformation = 'mag',
+                                       normalize = True,
+                                       sr=44100,
+                                       len_sec=1,
+                                       frame_length=512,
+                                       frame_step = 256,
+                                       fft_length = 1024,
+                                       n_mels = 40,
+                                       n_mfcc = 40,
+                                       mel_lower_edge=125,
+                                       mel_upper_edge=16384):
 
     if part == 'curated':
         df = pd.read_csv(os.path.join(path_ds,'train_curated.csv'))
@@ -100,7 +188,24 @@ def create_and_batch_dataset_freesound(path_ds, part='curated', batch_size=32, s
         labels = df['labels'].tolist()
 
     wave_path_ds = tf.data.Dataset.from_tensor_slices(names_paths)
-    wave_ds = wave_path_ds.map(load_and_preprocess_wav)
-    wave_ds = wave_ds.batch(batch_size)
+    label_ds = tf.data.Dataset.from_tensor_slices(labels)
 
-    return wave_ds
+    load_and_preprocess_rep = lambda _file: load_and_preprocess_wav(_file,
+                                            transformation,
+                                            normalize,
+                                            sr,
+                                            len_sec,
+                                            frame_length,
+                                            frame_step,
+                                            fft_length,
+                                            n_mels,
+                                            n_mfcc,
+                                            mel_lower_edge,
+                                            mel_upper_edge)
+
+    wave_ds = wave_path_ds.map(load_and_preprocess_rep)
+
+    wave_label_ds = tf.data.Dataset.zip((wave_ds,label_ds))
+    wave_label_ds = wave_label_ds.batch(batch_size)
+
+    return wave_label_ds
