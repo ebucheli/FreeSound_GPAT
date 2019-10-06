e30bfecd7aea2fd0f8240983d4da2375f048114f
ebucheli <edoardobucheli@gmail.com>
2019-10-05 19:45:54 -0500

    add __init__ for networks

---
+++ networks/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from networks.CNNetworks1D import *
+from networks.CNNetworks2D import *
+from networks.RNNetworks import *
+from networks.FFNetworks import *
