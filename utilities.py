27741b363396557367ad90493ed95f8fc29be24f
ebucheli <edoardobucheli@gmail.com>
2019-10-10 15:42:11 -0500

    tests

--- utilities.py
+++ utilities.py
@@ -4,6 +4,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+from tqdm import tqdm
+
 import json
 
 def plot_cm(y, y_hat, normalize = True, labels = None, figsize  = (15,15), xrotation = 0):
@@ -127,4 +129,88 @@ def get_x_and_labels(x, labels, meta_labels,cluster = 0):
     new_labels_mc.extend(labels_unk)
 
     return final_x, new_labels_mc, mc_new_label_mapping
-#def get_x_and_mc(x,):
+
+def apk(actual, predicted, k=10):
+    """
+    Computes the average precision at k.
+    This function computes the average prescision at k between two lists of
+    items.
+    Parameters
+    ----------
+    actual : list
+             A list of elements that are to be predicted (order doesn't matter)
+    predicted : list
+                A list of predicted elements (order does matter)
+    k : int, optional
+        The maximum number of predicted elements
+    Returns
+    -------
+    score : double
+            The average precision at k over the input lists
+    """
+    if len(predicted)>k:
+        predicted = predicted[:k]
+
+    score = 0.0
+    num_hits = 0.0
+
+    for i,p in enumerate(predicted):
+        if p in actual and p not in predicted[:i]:
+            num_hits += 1.0
+            score += num_hits / (i+1.0)
+
+    if not actual:
+        return 0.0
+
+    return score / min(len(actual), k)
+
+def mapk(actual, predicted, k=10):
+    """
+    Computes the mean average precision at k.
+    This function computes the mean average prescision at k between two lists
+    of lists of items.
+    Parameters
+    ----------
+    actual : list
+             A list of lists of elements that are to be predicted
+             (order doesn't matter in the lists)
+    predicted : list
+                A list of lists of predicted elements
+                (order matters in the lists)
+    k : int, optional
+        The maximum number of predicted elements
+    Returns
+    -------
+    score : double
+            The mean average precision at k over the input lists
+    """
+    return np.mean([apk(a,p,k) for a,p in zip(actual, predicted)])
+
+def evaluate_complete_files(x_test,y_test,model,input_shape):
+
+    all_scores = np.zeros((len(x_test),len(np.unique(y_test))))
+    scores_list = []
+
+    freq_res,frames = input_shape
+
+    for i,this_x in enumerate(tqdm(x_test)):
+
+        this_len = this_x.shape[1]
+        reps = int(np.floor(this_len/frames))
+
+        all_x = np.zeros((reps,freq_res,frames))
+
+        for j in range(reps):
+
+            start = j*frames
+            end = start+frames
+            my_x = this_x[:,start:end]
+
+            all_x[j] = my_x
+        y_hat_all = model.predict(all_x)
+        scores_list.append(y_hat_all)
+        y_hat_mean = np.mean(y_hat_all,axis = 0)
+
+        all_scores[i] = y_hat_mean
+
+    return all_scores, scores_list
